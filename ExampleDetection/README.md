bc762298bfbda081feebb754e23f1c3c45c5f189
shaun <shfaulds@microsoft.com>
2022-04-28 09:12:56 +1000

    example project to find a smaller segment inside a larger one

---
+++ ExampleDetection/README.md
@@ -0,0 +1,50 @@
+# ThemeServiceWebSite
+
+Example app to detect a theme Chromaprint in a larger Chromaprint of the first 15 min of a TV Show Episode.
+
+Using ffmpeg to extract the Chromaprint data.
+```
+C:\Install\ffmpeg\ffmpeg-5.0.1-full_build\bin\ffmpeg ^
+-i "C:\Data\media\StarTrek - Discovery - S03E07.mkv" ^
+-ss 00:07:47 ^
+-t 00:01:29 ^
+-ac 1 ^
+-acodec pcm_s16le ^
+-ar 16000 ^
+-c:v nul ^
+-f chromaprint ^
+-fp_format raw ^
+discovery_theme_01cmd.bin
+```
+This will extract the audio chromaprint data for a specific time period starting at 00:07:47 with a duration of 00:01:29
+In the TV Show episode I was looking at this was where the intro themem music was.
+```
+C:\Install\ffmpeg\ffmpeg-5.0.1-full_build\bin\ffmpeg ^
+-i "C:\Data\media\StarTrek - Discovery - S03E07.mkv" ^
+-ss 00:00:00 ^
+-t 00:15:00 ^
+-ac 1 ^
+-acodec pcm_s16le ^
+-ar 16000 ^
+-c:v nul ^
+-f chromaprint ^
+-fp_format raw ^
+discovery_15min.bin
+```
+This one does the same but for the first 15 minutes of the episode.
+
+You can then use the example in the project to "find" the offset of the first shorter segment inside the larger one, this gives you the start of the into theme.
+
+Checking your extraction theme music as a wav file.
+Use the following which is just the above but extracting the raw audio instead of the chromaprint to check you have the correct offsets when creating the chromaprint.
+```
+C:\Install\ffmpeg\ffmpeg-5.0.1-full_build\bin\ffmpeg ^
+-i "C:\Data\media\StarTrek - Discovery - S03E07.mkv" ^
+-ss 00:07:47 ^
+-t 00:01:29 ^
+-ac 1 ^
+-acodec pcm_s16le ^
+-ar 16000 ^
+-c:v nul ^
+discovery_theme_01.wav
+```
---
+++ ExampleDetection/ThemeDetection/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+
+string theme_cp_path = @"C:\Development\IntroSkip\ffmpeg\discovery_theme_01.bin";
+List<uint> theme_cp_data = LoadData(theme_cp_path);
+Console.WriteLine("Theme CP Lenth: " + theme_cp_data.Count);
+
+string episode_cp_path = @"C:\Development\IntroSkip\ffmpeg\discovery_15min.bin";
+List<uint> episode_cp_data = LoadData(episode_cp_path);
+Console.WriteLine("Episode CP Lenth: " + episode_cp_data.Count);
+
+List<uint> distances = new List<uint>();
+
+int last_offset = (episode_cp_data.Count - theme_cp_data.Count) + 1;
+for(int offset = 0; offset < last_offset; offset++)
+{
+	uint total_distance = 0;
+	for(int x = 0; x < theme_cp_data.Count; x++)
+    {
+		uint left = episode_cp_data[x + offset];
+		uint right = theme_cp_data[x];
+		uint this_score = GetHammingDist(left, right);
+		total_distance += this_score;
+	}
+	distances.Add(total_distance);
+}
+
+Console.WriteLine(distances.Count);
+WriteResult(distances, @"C:\Development\IntroSkip\ffmpeg\distances.tsv");
+
+uint sum_distances = 0;
+uint min_dist = uint.MaxValue;
+int min_offset = -1;
+for(int x = 0; x < distances.Count; x++)
+{
+	sum_distances += distances[x];
+	if (distances[x] < min_dist)
+    {
+		min_dist = distances[x];
+		min_offset = x;
+	}
+}
+double average_distance = sum_distances / distances.Count;
+Console.WriteLine("Min Distance        : " + min_dist);
+Console.WriteLine("Average Distance    : " + average_distance);
+Console.WriteLine("Min Distance Offset : " + min_offset);
+
+// based on testing it looks like it is about 8.06 ints per second based on the options I used in the ffmpeg audio flattening
+int theme_start = (int)(min_offset / 8.06);
+TimeSpan ts = new TimeSpan(0, 0, theme_start);
+Console.WriteLine("Theme At : " + ts);
+
+uint GetHammingDist(uint left, uint right)
+{
+	uint distance = left ^ right;
+	distance = ((distance >> 1) & 1431655765U) + (distance & 1431655765U);
+	distance = ((distance >> 2) & 858993459U) + (distance & 858993459U);
+	distance = ((distance >> 4) & 252645135U) + (distance & 252645135U);
+	distance = ((distance >> 8) & 16711935U) + (distance & 16711935U);
+	distance = ((distance >> 16) & 65535U) + (distance & 65535U);
+	return distance;
+}
+
+void WriteResult(List<uint> distances, string path)
+{
+	using (FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
+	{
+		foreach (uint dist in distances)
+		{
+			fileStream.Write(Encoding.UTF8.GetBytes(dist.ToString() + "\n"));
+		}
+	}
+}
+
+List<uint> LoadData(string cp_path)
+{
+	List<uint> cp_data = new List<uint>();
+	using (FileStream fileStream = new FileStream(cp_path, FileMode.Open, FileAccess.Read, FileShare.Read))
+	{
+		using (BinaryReader binaryReader = new BinaryReader(fileStream))
+		{
+			int num = (int)binaryReader.BaseStream.Length / 4;
+			for (int i = 0; i < num; i++)
+			{
+				cp_data.Add(binaryReader.ReadUInt32());
+			}
+		}
+	}
+	return cp_data;
+}
+
